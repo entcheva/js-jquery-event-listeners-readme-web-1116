831bf7cca571624b4d85e117a457da754ad81099
entcheva <christina.entcheva@gmail.com>
2017-01-18 18:41:17 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -1,5 +1,27 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function() {
+    alert('you clicked!');
+  })
+};
+
+function frameIt() {
+
+};
+
+function submitIt() {
+
+};
+
+function pressIt() {
+
+};
+
+
+
+
+
 $(document).ready(function(){
 
 // call functions here
