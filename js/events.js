1757064b8fb272e31a29dd252efce7b74505ccf7
entcheva <christina.entcheva@gmail.com>
2017-01-21 23:23:56 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -1,29 +1,32 @@
-//define functions here
-
 function getIt() {
   $('p').on('click', function() {
-    alert('you clicked!');
+    alert('you clicked!')
   })
-};
+}
 
 function frameIt() {
-
-};
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
 
 function submitIt() {
-
-};
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
 
 function pressIt() {
-
-};
-
-
-
-
+  $('form').on('keydown', function(key) {
+    if (key.which == 103 || key.which == 71) {
+      alert("you pressed g")
+    }
+  })
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt()
+  frameIt()
+  submitIt()
+  pressIt()
 });
